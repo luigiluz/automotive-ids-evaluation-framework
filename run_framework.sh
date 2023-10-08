817c164071cfa06d920b2d6d13e926b263f4320b
Luigi Luz <lfml@cin.ufpe.br>
2023-10-08 00:40:08 -0300

    refactor: update model_train_validation script

--- run_framework.sh
+++ run_framework.sh
@@ -2,34 +2,38 @@
 
 # Paths
 BASE_PATH="/home/lfml/workspace/automotive-ids-evaluation-framework"
-FEAT_GEN_CONFIG_FOLDER="config_jsons/feat_gen"
-MODEL_CONFIG_FOLDER="config_jsons/model"
-PRESAVED_MODELS_FOLDER="config_jsons/presaved_files"
+FEAT_GEN_CONFIG_FOLDER="config_jsons/feat_generator"
+MODEL_TRAIN_VALID_CONFIG_FOLDER="config_jsons/model_train_validate"
+MODEL_TEST_CONFIG_FOLDER="config_jsons/model_test"
+MULTI_STAGE_CONFIG_FOLDER="config_jsons/multi_stage_ids"
 
 # Change this according to the desired configuration
 ## Feat generator configs
-# SELECTED_FEAT_GEN_CONFIG="cnnids_tow_config_maidai.json"
-SELECTED_FEAT_GEN_CONFIG="pruned_config_maidai.json"
+SELECTED_FEAT_GEN_CONFIG="AVTP_CNNIDS_train.json"
 
-## Model configs
-# SELECTED_MODEL_CONFIG="cnnids_model_maidai.json"
-# SELECTED_MODEL_CONFIG="multiclass_cnnids_model_maidai.json"
-# SELECTED_MODEL_CONFIG="pruned_model_maidai.json"
-SELECTED_MODEL_CONFIG="random_forest_params.json"
+## Model train validate configs
+SELECTED_MODEL_TRAIN_VALIDATE_CONFIG="AVTP_CNNIDS_train.json"
 
-## Presaved configs
-SELECTED_PRESAVED_MODELS="multiclass_cnn.json"
+## Model test config
+SELECTED_MODEL_TEST_CONFIG="AVTP_CNNIDS_test.json"
+
+## Multi stage ids config
+SELECTED_MULTI_STAGE_IDS_CONFIG="AVTP_MultiStage.json"
 
 # DO NOT CHANGE FROM THIS POINT ON
 FEAT_GEN_CONFIG_PATH="${BASE_PATH}/${FEAT_GEN_CONFIG_FOLDER}/${SELECTED_FEAT_GEN_CONFIG}"
-MODEL_CONFIG_PATH="${BASE_PATH}/${MODEL_CONFIG_FOLDER}/${SELECTED_MODEL_CONFIG}"
-PRESAVED_MODELS_PATH="${BASE_PATH}/${PRESAVED_MODELS_FOLDER}/${SELECTED_PRESAVED_MODELS}"
+MODEL_TRAIN_VALID_CONFIG_PATH="${BASE_PATH}/${MODEL_TRAIN_VALID_CONFIG_FOLDER}/${SELECTED_MODEL_TRAIN_VALIDATE_CONFIG}"
+MODEL_TEST_CONFIG_PATH="${BASE_PATH}/${MODEL_TEST_CONFIG_FOLDER}/${SELECTED_MODEL_TEST_CONFIG}"
+MULTI_STAGE_CONFIG_PATH="${BASE_PATH}/${MULTI_STAGE_CONFIG_FOLDER}/${SELECTED_MULTI_STAGE_IDS_CONFIG}"
 
 # Run the feature generator step
 # venv/bin/python3 execute_feature_generator.py --feat_gen_config ${FEAT_GEN_CONFIG_PATH}
 
 # Run the model training and validation step
-venv/bin/python3 execute_model_train_validation.py --feat_gen_config ${FEAT_GEN_CONFIG_PATH} --model_hyperparams ${MODEL_CONFIG_PATH}
+venv/bin/python3 execute_model_train_validation.py --model_train_valid_config ${MODEL_TRAIN_VALID_CONFIG_PATH}
 
 # Run the model test step
-# venv/bin/python3 execute_model_test.py --feat_gen_config ${FEAT_GEN_CONFIG_PATH} --model_hyperparams ${MODEL_CONFIG_PATH} --presaved ${PRESAVED_MODELS_PATH}
+# venv/bin/python3 execute_model_test.py --model_test_config ${MODEL_TEST_CONFIG_PATH}
+
+# Run the multi stage method
+# venv/bin/python3 execute_multi_stage_ids.py --multi_stage_config ${MULTI_STAGE_CONFIG_PATH}
